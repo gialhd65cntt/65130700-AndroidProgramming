70d471dc0b69895a7b38ab42f9fb3639bd8c6288
Teddy <danggia22112005@gmail.com>
2026-03-15 14:35:55 +0700

    hoàn thiện bài tính tổng

--- BaiTH2/app/src/main/java/ntu/edu/appcong/MainActivity.java
+++ BaiTH2/app/src/main/java/ntu/edu/appcong/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu.edu.appcong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+    //Xử lý sự kiện nút tính tổng
+    public void XuLyCong(View view){
+        // tìm, tham chiếu đến điều khiển trên xml
+       EditText editTextSoA =findViewById(R.id.edtA);
+       EditText editTextSoB =findViewById(R.id.edtB);
+       EditText editTextKetQua =findViewById(R.id.edtKQ);
+       // lấy dữ liệu về ở đk số a
+       String strA = editTextSoA.getText().toString();
+        // lấy dữ liệu về ở đk số b
+       String strB = editTextSoB.getText().toString();
+    // chuyển dữ liệu sang số
+        int soA=Integer.parseInt(strA);
+        int soB=Integer.parseInt(strB);
+    // tính toán theo yêu cầu
+        int tong= soA+soB;
+        String strTong=String.valueOf(tong); // chuyển sang dạng chuỗi
+        // in ra màn hình
+        editTextKetQua.setText(strTong);
     }
 }
